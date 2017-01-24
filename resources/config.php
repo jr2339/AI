b1d874ff8d2c7fccf39f696d234fdb3aef99e2c1
jr2339 <jr2339@nau.edu>
2017-01-24 14:52:58 -0700

    Finished define path with Constants

--- resources/config.php
+++ resources/config.php
@@ -10,10 +10,11 @@ defined("DS") ? null:define("DS",DIRECTORY_SEPARATOR);
 
 //Define Root
 
-
-echo __DIR__;
-
-
+defined("TEMPLATE_FRONT") ? null:define("TEMPLATE_FRONT",__DIR__.DS."templates/front");
+defined("TEMPLATE_BACK") ? null:define("TEMPLATE_BACK",__DIR__.DS."templates/back");
+echo TEMPLATE_FRONT;
+echo "<br>";
+echo TEMPLATE_BACK;
 
 
 
