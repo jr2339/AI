f05d9b04807f047f4c2e508f2d8c2eb20eb81ed2
jr2339 <jr2339@nau.edu>
2017-01-27 14:27:04 -0700

    Connect to the Database

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,8 +1,21 @@
 <div class="col-md-3">
-    <p class="lead">Shop Name</p>
+    <p class="lead">Categories</p>
     <div class="list-group">
+
+        <?php
+            $query ="SELECT * FROM categories";
+            $send_query = mysqli_query($connection,$query);
+            if(!$send_query){
+                die("QUERY FAILED".mysqli_error($connection));
+            }
+            while($row = mysqli_fetch_array($send_query)){
+                echo "<a href=' ' class='list-group-item'>{$row['cat_title']}</a>";
+            }
+        ?>
+        <!----------------This is the code from templates------------
         <a href="category.html" class="list-group-item">Category 1</a>
         <a href="#" class="list-group-item">Category 2</a>
         <a href="#" class="list-group-item">Category 3</a>
+        ------------------------------------------------------------->
     </div>
 </div>
