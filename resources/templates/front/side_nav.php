52ddb62ec405b38c5bd0323f58b6c399c7a6acd2
jr2339 <jr2339@nau.edu>
2017-01-30 11:45:11 -0700

    Custom Helper Function

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -3,6 +3,7 @@
     <div class="list-group">
 
         <?php
+            //SQL statement here
             $query ="SELECT * FROM categories";
             $send_query = mysqli_query($connection,$query);
             if(!$send_query){
@@ -12,6 +13,7 @@
                 echo "<a href=' ' class='list-group-item'>{$row['cat_title']}</a>";
             }
         ?>
+
         <!----------------This is the code from templates------------
         <a href="category.html" class="list-group-item">Category 1</a>
         <a href="#" class="list-group-item">Category 2</a>
