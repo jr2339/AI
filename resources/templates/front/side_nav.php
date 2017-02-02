dc8519cc9d1135ef9bc8adfbe3a5fb100474ff49
jr2339 <jr2339@nau.edu>
2017-02-02 14:56:16 -0700

    Category Page Function

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -3,15 +3,8 @@
     <div class="list-group">
 
         <?php
-            //SQL statement here
-            $query ="SELECT * FROM categories";
-            $send_query = mysqli_query($connection,$query);
-            if(!$send_query){
-                die("QUERY FAILED".mysqli_error($connection));
-            }
-            while($row = mysqli_fetch_array($send_query)){
-                echo "<a href=' ' class='list-group-item'>{$row['cat_title']}</a>";
-            }
+            get_categories();
+
         ?>
 
         <!----------------This is the code from templates------------
