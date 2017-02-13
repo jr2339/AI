86548d0d075656ae25c9cdd6960e9c0a416ac604
jr2339 <jr2339@nau.edu>
2017-02-13 13:27:26 -0700

    Cart Function

--- public/cart.php
+++ public/cart.php
@@ -50,7 +50,28 @@
         redirect("checkout.php");
     }
 
+    function cart(){
+        $query = query("SELECT * FROM products");
+        confirm($query);
+        while($row = fetch_array($query)){
+$product = <<<DELIMETER
 
+
+ <tr>
+    <td>{$row['product_title']}</td>
+    <td>$23</td>
+    <td>3</td>
+    <td>2</td>
+    <td><a href="cart.php?remove=1">Remove</a></td>
+    <td><a href="cart.php?delete=1">Delete</a></td>
+</tr>
+
+DELIMETER;
+
+    echo $product;
+
+        }
+    }
 
 
 
